df8c8fa967d4ac1cc8639ba82eb9061256da7182
Hong-Jie Dai <hongjiedai@gmail.com>
2017-07-14 16:51:38 +0800

    修正 \n 的輸出

--- src/main/java/BigoDM/PPT_MAE_Generator/PregnancyCorpusConverter.java
+++ src/main/java/BigoDM/PPT_MAE_Generator/PregnancyCorpusConverter.java
@@ -19,37 +19,42 @@ import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
 
 /**
- * https://cliftonlabs.github.io/json-simple/
- * http://dom4j.sourceforge.net/dom4j-1.6.1/index.html
  * 
- * @author Dai
- *
+ * 
+ * @author Frank and Dai
+ * 
  */
-public class test2 {
+public class PregnancyCorpusConverter {
 
 	public static void main(String[] argv) throws IOException {
 		try {
-			JsonReader jsonReader = Json
-					.createReader(new InputStreamReader(new FileInputStream("sample.json"), "Big5"));// UTF8
-			// "BabyMother-1-5719.json"), "UTF-8"));// UTF8
+			System.out.println("Reading file...");
+			JsonReader jsonReader = Json.createReader(new InputStreamReader(
+					new FileInputStream(// "sample.json"), "Big5"));// UTF8
+							"BabyMother-1-5955.json"), "UTF-8"));// UTF8
 			// 編碼
+			System.out.println("Reading json objects...");
 			JsonObject jsonObject = jsonReader.readObject();
+			System.out.println("Rtriving json arrays...");
 			JsonArray jsonArray = jsonObject.getJsonArray("articles");
-			// ?U???O????C?@????
+			System.out.println("Converting");
 			for (JsonValue value : jsonArray) {
 				JsonObject article = (JsonObject) value;
 				String article_id = (String) article.getString("article_id");
-				String article_title = (String) article.getString("article_title");
-				String content = (String) article.getString("content");
-				Document doc = DocumentHelper.createDocument();// ????
+				String article_title = (String) article.getString(
+						"article_title").replace("\\n", "\n");
+				String content = (String) article.getString("content").replace(
+						"\\n", "\n");
+				Document doc = DocumentHelper.createDocument();
 				Element root = doc.addElement("PTTCorpus");// <PTTCorpus>
 				Element second = root.addElement("TEXT");// <TEXT>
-				CDATA cdata = DocumentHelper.createCDATA("標題: " + article_title + "\n內文:\n" + content);
+				CDATA cdata = DocumentHelper.createCDATA("標題: " + article_title
+						+ "\n內文:\n" + content);
 				second.add(cdata);
 				FileWriter fw = new FileWriter(article_id + ".xml");
-				OutputFormat of = new OutputFormat(); // ?榡??XML
-				of.setIndentSize(4); // ?]?w Tab ?? 4 ????
-				of.setNewlines(true);// ?]?w ??????
+				OutputFormat of = new OutputFormat();
+				of.setIndentSize(4);
+				of.setNewlines(true);
 				XMLWriter xw = new XMLWriter(fw, of);
 				xw.write(doc);
 				xw.close();
